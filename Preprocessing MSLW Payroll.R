210c95162c9e6f5a28368652daf4072ced5096aa
Weber <anjelica.weber@mssm.edu>
2020-04-30 16:18:06 -0400

    Adding to preprocessing data code section

--- Preprocessing MSLW Payroll.R
+++ Preprocessing MSLW Payroll.R
@@ -5,11 +5,19 @@ options(java.parameters = "-Xmx8000m")
 
 # Importing Dictionaries --------------------------------------------------
 path_dict_pc_alt <- file.choose(new = T)
-dict_paycycle_alt <- read.xlsx(path_dict_pc_alt, sheetIndex = 1)
+dict_paycycle_alt <- read_excel(path_dict_pc_alt)
+colnames(dict_paycycle_alt) <- c("Start.Date","End.Date", "Start-End","Start 1","End 1","Start 2","End 2" )
 
 # Importing Data ----------------------------------------------------------
 path_data_1 <- file.choose(new = T)
 path_data_2 <- file.choose(new = T)
-#data_1 <- read.xlsx(path_data_1, sheetIndex = 1)
-#data_1 <- read_excel(path_data_1)
-#data_2 <- read.xlsx(path_data_2, sheetIndex = 1)
+data_1 <- read.csv(file = path_data_1, sep = ",")
+data_2 <- read.csv(file = path_data_2, sep = ",")
+
+# Preprocessing Data ------------------------------------------------------
+data_1$`Start-End` <- paste0(data_1$Start.Date, "-", data_1$End.Date)
+data_2$`Start-End` <- paste0(data_2$Start.Date, "-", data_2$End.Date)
+data_1$Start.Date <- as.Date(data_1$Start.Date, "%m/%d/%Y")
+data_1$End.Date <- as.Date(data_1$End.Date, "%m/%d/%Y")
+data_2$Start.Date <- as.Date(data_2$Start.Date, "%m/%d/%Y")
+data_2$End.Date <- as.Date(data_2$End.Date, "%m/%d/%Y")
