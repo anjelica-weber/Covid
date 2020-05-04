b53b3d7e62c8f49896dfd5cff6614f14f37cda3e
Weber <anjelica.weber@mssm.edu>
2020-05-04 16:09:36 -0400

    Converting code to reference files from sharedrive

--- MSLW Payroll.R
+++ MSLW Payroll.R
@@ -1,38 +1,27 @@
-dir <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement"
+#dir <- "J:/deans/Presidents/SixSigma/MSHS Productivity/Productivity/Analysis/FEMA Reimbursement/MSHS-FEMA-Reimbursement"
 setwd(dir)
 
 # Load Libriaries ---------------------------------------------------------
 library(readxl)
-#library(tidyverse)
+library(tidyverse)
 #library(dplyr)
 
 # Import Data -------------------------------------------------------------
-folder_data <- paste0(dir,"/MSLW RAW")
-list_data_files <- list.files(folder_data, pattern = "rds$", full.names = T)
-list_data <- lapply(list_data_files, read_excel)
-merge_multiple_dataframes<- function(list.dfs){
-  output<- list.dfs[1]
-  for (i in 2:length(list.dfs)){
-    output <- merge.data.frame(output, list.dfs[i], all.x = T, all.y = T)
-  }
-  return(output)
-}
-data_mslw <- merge_multiple_dataframes(list_data)
+data_MSSL_MSW <- readRDS(paste0(dir,"/MSLW RAW/Data_MSSL_MSW.rds"))
 
 # Preprocess Data ------------------------------------------------------
-data_mslw_test$START.DATE <- sapply(data_mslw_test$START.DATE, function(x) paste0(substr(x,1,2),"/", substr(x,3,4),"/", substr(x,5,8)))
-data_mslw_test$END.DATE <- sapply(data_mslw_test$END.DATE, function(x) paste0(substr(x,1,2),"/", substr(x,3,4),"/", substr(x,5,8)))
-data_mslw_test$START.DATE <- as.Date(data_mslw_test$START.DATE, "%m/%d/%Y")
-data_mslw_test$END.DATE <- as.Date(data_mslw_test$END.DATE, "%m/%d/%Y")
+data_MSSL_MSW <- data_MSSL_MSW %>% mutate(`START DATE` = paste0(substr(`START DATE`,1,2), "/", substr(`START DATE`,3,4), "/",substr(`START DATE`,5,8)),
+                                          `START DATE` = as.Date(`START DATE`, "%m/%d/%Y"),
+                                          `END DATE` = paste0(substr(`END DATE`,1,2), "/", substr(`END DATE`,3,4), "/",substr(`END DATE`,5,8)),
+                                          `END DATE` = as.Date(`END DATE`, "%m/%d/%Y"))
 
 # Import References -------------------------------------------------------
-folder_references <- paste0(dir,"/Reference Tables")
+folder_references <- paste0(dir,"/MSLW Reference Tables")
 dict_paycycle_alt <- read_xlsx(paste0(folder_references, "/Dictionary_Alt Pay Cycles.xlsx"))
 dict_paycycle_sys <- read.csv(paste0(folder_references,"/PayCycle.csv"), header = T, stringsAsFactors = F)
 dict_jobcodes <- read_xlsx(paste0(folder_references, "/MSLW Job Codes.xlsx"))
 dict_COFTloc <- read_xlsx(paste0(folder_references, "/Dictionary_COFT.xlsx")) #this will be from matts excel file
 dict_site <- read_xlsx(paste0(folder_references, '/Dictionary_Site.xlsx'))
-#dict_paycodes <- read_xlsx(folder_references, "/All Sites Pay Code Mappings.xlsx")
 
 # Splitting Biweekly 2 Pay Cycle ------------------------------------------
 format_biweekly_paycycle <- function(dfs){
@@ -52,32 +41,32 @@ format_biweekly_paycycle <- function(dfs){
   dfs_final <- rbind(dfs_other, dfs_alt_1, dfs_alt_2)
   return(dfs_final)
   }
-data_mslw_test <- format_biweekly_paycycle(data_mslw_test)
+data_MSSL_MSW <- format_biweekly_paycycle(data_MSSL_MSW)
 
 # Lookup Jobcodes ---------------------------------------------------------
-data_mslw_test <- merge.data.frame(data_mslw_test, dict_jobcodes, all.x = T)
-colnames(data_mslw_test)[which("Position.Code.Description"==colnames(data_mslw_test))] <- "J.C.DESCRIPTION"
+data_MSSL_MSW <- merge.data.frame(data_MSSL_MSW, dict_jobcodes, all.x = T)
+colnames(data_MSSL_MSW)[which("Position.Code.Description"==colnames(data_MSSL_MSW))] <- "J.C.DESCRIPTION"
 
 # Lookup Location ---------------------------------------------------------
-data_mslw_test <- merge.data.frame(data_mslw_test, dict_COFTloc, by.x = "WD_COFT", by.y ="COFT" , all.x = T)
-colnames(data_mslw_test)[which("COFT_LOC_GROUP"==colnames(data_mslw_test))] <- 'WRKD.LOCATION'
-data_mslw_test <- merge.data.frame(data_mslw_test, dict_COFTloc, by.x = "HD_COFT", by.y ="COFT" , all.x = T)
-colnames(data_mslw_test)[which("COFT_LOC_GROUP"==colnames(data_mslw_test))] <- 'HOME.LOCATION'
+data_MSSL_MSW <- merge.data.frame(data_MSSL_MSW, dict_COFTloc, by.x = "WD_COFT", by.y ="COFT" , all.x = T)
+colnames(data_MSSL_MSW)[which("COFT_LOC_GROUP"==colnames(data_MSSL_MSW))] <- 'WRKD.LOCATION'
+data_MSSL_MSW <- merge.data.frame(data_MSSL_MSW, dict_COFTloc, by.x = "HD_COFT", by.y ="COFT" , all.x = T)
+colnames(data_MSSL_MSW)[which("COFT_LOC_GROUP"==colnames(data_MSSL_MSW))] <- 'HOME.LOCATION'
 
 # Cost Center ("Department") ---------------------------------------------
-data_mslw_test$DPT.WRKD <- paste0(data_mslw_test$WD_COFT, data_mslw_test$WD_Location, data_mslw_test$WD_Department)
-data_mslw_test$DPT.HOME <- paste0(data_mslw_test$HD_COFT, data_mslw_test$HD_Location, data_mslw_test$HD_Department)
+data_MSSL_MSW$DPT.WRKD <- paste0(data_MSSL_MSW$WD_COFT, data_MSSL_MSW$WD_Location, data_MSSL_MSW$WD_Department)
+data_MSSL_MSW$DPT.HOME <- paste0(data_MSSL_MSW$HD_COFT, data_MSSL_MSW$HD_Location, data_MSSL_MSW$HD_Department)
 
 # Lookup Site -------------------------------------------------------------
-data_mslw_test <- merge.data.frame(data_mslw_test,dict_site, by.x = "Home.FacilityOR.Hospital.ID", by.y = 'Site ID', all.x = T)
-colnames(data_mslw_test)[which("Site"==colnames(data_mslw_test))] <- 'HOME.SITE'
-data_mslw_test <- merge.data.frame(data_mslw_test,dict_site, by.x = "Facility.Hospital.Id_Worked", by.y = 'Site ID', all.x = T)
-colnames(data_mslw_test)[which("Site"==colnames(data_mslw_test))] <- 'WRKD.SITE'
+data_MSSL_MSW <- merge.data.frame(data_MSSL_MSW,dict_site, by.x = "Home.FacilityOR.Hospital.ID", by.y = 'Site ID', all.x = T)
+colnames(data_MSSL_MSW)[which("Site"==colnames(data_MSSL_MSW))] <- 'HOME.SITE'
+data_MSSL_MSW <- merge.data.frame(data_MSSL_MSW,dict_site, by.x = "Facility.Hospital.Id_Worked", by.y = 'Site ID', all.x = T)
+colnames(data_MSSL_MSW)[which("Site"==colnames(data_MSSL_MSW))] <- 'WRKD.SITE'
 
 # Rename/Format Columns ---------------------------------------------------
-colnames(data_mslw_test)[which("Hours"==colnames(data_mslw_test))] <- 'HOURS'
-colnames(data_mslw_test)[which("Expense"==colnames(data_mslw_test))] <- 'EXPENSE'
-colnames(data_mslw_test)[which("Department.Name.Worked.Dept"==colnames(data_mslw_test))] <- "WRKD.DESCRIPTION"
-colnames(data_mslw_test)[which("Department.Name.Home.Dept"==colnames(data_mslw_test))] <- "HOME.DESCRIPTION"
-colnames(data_mslw_test)[which('Pay.Code'==colnames(data_mslw_test))] <- "PAY.CODE"
-colnames(data_mslw_test)[which('Employee.ID'==colnames(data_mslw_test))] <- "LIFE"
+colnames(data_MSSL_MSW)[which("Hours"==colnames(data_MSSL_MSW))] <- 'HOURS'
+colnames(data_MSSL_MSW)[which("Expense"==colnames(data_MSSL_MSW))] <- 'EXPENSE'
+colnames(data_MSSL_MSW)[which("Department.Name.Worked.Dept"==colnames(data_MSSL_MSW))] <- "WRKD.DESCRIPTION"
+colnames(data_MSSL_MSW)[which("Department.Name.Home.Dept"==colnames(data_MSSL_MSW))] <- "HOME.DESCRIPTION"
+colnames(data_MSSL_MSW)[which('Pay.Code'==colnames(data_MSSL_MSW))] <- "PAY.CODE"
+colnames(data_MSSL_MSW)[which('Employee.ID'==colnames(data_MSSL_MSW))] <- "LIFE"
